5f9a6a476e511ce9cfe46a3ac26b9c8d8b3ecd5b
manovasamuel <121334706+manovasamuel@users.noreply.github.com>
2025-10-01 12:58:30 +0530

    Update todo.js
    new version of to do and used new logic

--- todo.js
+++ todo.js
@@ -1,35 +1,66 @@
-"user strict";
+"use strict";
 
-const todoEl = document.getElementById("todoValue");
+const todoInput = document.querySelector(".input_val");
+const unl = document.querySelector("ul");
+const addTodo = document.querySelector(".addbtn");
+const clearbtn = document.querySelector(".clear");
+const resetbtn = document.querySelector(".reset");
+const savebtn = document.querySelector(".save");
 
-const btn = document.querySelector(".addbtn");
-const deletebtn = () => {
-  const delbtn = document.querySelector(".deletebtn");
-  delbtn.addEventListener("click", function (e) {
+const showCustomAlert = (message) => {
+  const customAlert = document.getElementById("customAlert");
+  customAlert.textContent = message;
+  customAlert.classList.add("show");
+
+  // Hide the alert after 3 seconds
+  setTimeout(() => {
+    customAlert.classList.remove("show");
+  }, 3000);
+};
+
+const createTodo = () => {
+  const lis = document.createElement("li");
+  const inp = document.createElement("input");
+  let randomId = Math.floor(Math.random() * 1000);
+  inp.id = randomId;
+  inp.type = "checkbox";
+  inp.className = "check";
+  const label = document.createElement("label");
+  label.htmlFor = randomId;
+  label.textContent = todoInput.value;
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "X";
+
+  lis.append(inp);
+  lis.append(label);
+  lis.append(delBtn);
+  unl.append(lis);
+
+  if (todoInput.value === "") {
+    showCustomAlert("Please enter a task");
+    lis.remove();
+    return;
+  }
+
+  // functions
+
+  const delToddo = (e) => {
     e.target.parentElement.remove();
-  });
-};
-const strikeout = () => {
-  const checkbbox = document.getElementById("box");
-  checkbbox.addEventListener("click", function (e) {
-    e.target.nextElementSibling.classList.toggle("strike");
-  });
-};
-const todo = function () {
-  const tohtml = `
-   <div>
-      <input type="checkbox" id="box" />
-      <label for="box" class="boxvalue">${todoEl.value}</label>
-      }</label>
-      <button class="deletebtn">-</button>
-    </div>
-  `;
+  };
+  const check_todo = () => {
+    label.classList.toggle("checked");
+  };
 
-  todoEl.value === ""
-    ? alert("Please enter a task")
-    : document.body.insertAdjacentHTML("afterend", tohtml);
-  deletebtn();
-  strikeout();
-  todoEl.value = "";
+  // Event handlers
+  delBtn.addEventListener("click", delToddo);
+
+  inp.addEventListener("click", check_todo);
+
+  todoInput.value = "";
 };
-btn.addEventListener("click", todo);
+
+const clearAllTodo = () => {
+  unl.innerHTML = "";
+};
+clearbtn.addEventListener("click", clearAllTodo);
+addTodo.addEventListener("click", createTodo);
